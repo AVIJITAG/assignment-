8d9b784c88f6215b7d2d7e8dc086282ad6277fbf
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-09-02 12:38:32 +0530

    Add files via upload

---
+++ August/MyProgram2.java
@@ -0,0 +1,21 @@
+//Fix the compilation error and invoke the method printMessage() of DisplayMessage class.
+
+
+
+public class MyProgram2 {
+
+		  public static void main(String[] args) {
+		  DisplayMessage obj = new DisplayMessage();
+		    
+              obj.printMessage();
+		 
+         }
+		}
+		
+              class DisplayMessage {
+		  void printMessage() {
+		   
+              System.out.println("Hello My Message");
+		  
+        }
+		}
