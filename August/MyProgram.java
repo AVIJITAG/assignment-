6c770af42159a82536cce2b29cc8a1a98caab7b1
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-09-02 11:03:25 +0530

    Add files via upload

---
+++ August/MyProgram.java
@@ -0,0 +1,22 @@
+/* Problem Statement:
+   
+   Fix the compilation errors and run the program? The program should print “My First Java Program” in the console.
+
+   Class  myProgram {
+   Public static void main(String arg[]);{
+   system.out.Println("My First Java Program");
+   }
+   }
+
+   Save the file as “MyProgram.java” and compile it  */
+  
+ 
+      public class MyProgram {
+
+	public static void main(String[] args) {
+	
+      System.out.println("My First Java Program");
+	
+}
+ }
+
